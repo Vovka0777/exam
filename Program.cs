4dcd54afdbe7a13a14f2436e18fc1f3faabe0e0a
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-06-27 09:16:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Point<int> intPoint = new Point<int>(-10, 20);
+        intPoint.Print();
+
+        Point<uint> uintPoint = new Point<uint>(30, 40);
+        uintPoint.Print();
+
+        Point<double> doublePoint = new Point<double>(-12.5, 34.7);
+        doublePoint.Print();
+    }
+}
