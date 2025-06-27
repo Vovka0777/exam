face32c5f270c228a470a16426eee0612b0e9906
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-06-27 09:07:11 +0300

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Point<T>
+{
+    T x;
+    T y;
+
+    public T X { get => x; set => x = value; }
+    public T Y { get => y; set => y = value; }
+    public Point(T x, T y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Координаты точки: X = {X}, Y = {Y}");
+    }
+}
